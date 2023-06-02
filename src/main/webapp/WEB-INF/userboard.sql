d373037821c3d3e66c74c06bcb06cf24c446544e
home <objet_an@naver.com>
2023-06-03 00:10:28 +0900

    SQL 파일 업로드

---
+++ src/main/webapp/WEB-INF/userboard.sql
@@ -0,0 +1,206 @@
+LastSessions<|||>1<|||>gdj11
+LastActiveSession<|||>1<|||>gdj11
+SessionManager_WindowLeft<|||>3<|||>229
+SessionManager_WindowTop<|||>3<|||>119
+ColWidths_connform.ListSessions<|||>1<|||>163,50,50,50,50,50,10
+ColsVisible_connform.ListSessions<|||>1<|||>0,1,4,6
+ColPositions_connform.ListSessions<|||>1<|||>0,1,2,3,4,5,6
+ColSort_connform.ListSessions<|||>1<|||>0,0
+CreateDbCollation<|||>1<|||>utf8_general_ci
+MainWinOnMonitor<|||>3<|||>0
+ColWidths_MainForm.ListDatabases<|||>1<|||>150,80,50,50,50,50,50,50,50,50,120
+ColsVisible_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
+ColPositions_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
+ColSort_MainForm.ListDatabases<|||>1<|||>0,0
+ColWidths_MainForm.ListVariables<|||>1<|||>160,200,428
+ColsVisible_MainForm.ListVariables<|||>1<|||>0,1,2
+ColPositions_MainForm.ListVariables<|||>1<|||>0,1,2
+ColSort_MainForm.ListVariables<|||>1<|||>0,0
+ColWidths_MainForm.ListStatus<|||>1<|||>160,428,100,100
+ColsVisible_MainForm.ListStatus<|||>1<|||>0,1,2,3
+ColPositions_MainForm.ListStatus<|||>1<|||>0,1,2,3
+ColSort_MainForm.ListStatus<|||>1<|||>0,0
+ColWidths_MainForm.ListProcesses<|||>1<|||>70,80,80,80,80,50,50,197
+ColsVisible_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
+ColPositions_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
+ColSort_MainForm.ListProcesses<|||>1<|||>0,1
+ColWidths_MainForm.ListCommandStats<|||>1<|||>120,100,100,100,368
+ColsVisible_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
+ColPositions_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
+ColSort_MainForm.ListCommandStats<|||>1<|||>1,1
+ColWidths_MainForm.ListTables<|||>1<|||>120,70,70,120,120,70,100,50,70,70,70,70,70,90,120,70,70,70,50
+ColsVisible_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,18
+ColPositions_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
+ColSort_MainForm.ListTables<|||>1<|||>0,0
+FontSize<|||>3<|||>11
+SQL Attr Comment Foreground<|||>3<|||>8421504
+SQL Attr Comment Background<|||>3<|||>536870911
+SQL Attr Comment Style<|||>3<|||>2
+SQL Attr ConditionalComment Foreground<|||>3<|||>8421504
+SQL Attr ConditionalComment Background<|||>3<|||>536870911
+SQL Attr ConditionalComment Style<|||>3<|||>2
+SQL Attr ConsoleOutput Foreground<|||>3<|||>536870911
+SQL Attr ConsoleOutput Background<|||>3<|||>536870911
+SQL Attr ConsoleOutput Style<|||>3<|||>5
+SQL Attr DataType Foreground<|||>3<|||>128
+SQL Attr DataType Background<|||>3<|||>536870911
+SQL Attr DataType Style<|||>3<|||>1
+SQL Attr DefaultPackages Foreground<|||>3<|||>536870911
+SQL Attr DefaultPackages Background<|||>3<|||>536870911
+SQL Attr DefaultPackages Style<|||>3<|||>1
+SQL Attr DelimitedIdentifier Foreground<|||>3<|||>32896
+SQL Attr DelimitedIdentifier Background<|||>3<|||>536870911
+SQL Attr DelimitedIdentifier Style<|||>3<|||>0
+SQL Attr Exception Foreground<|||>3<|||>536870911
+SQL Attr Exception Background<|||>3<|||>536870911
+SQL Attr Exception Style<|||>3<|||>2
+SQL Attr Function Foreground<|||>3<|||>8388608
+SQL Attr Function Background<|||>3<|||>536870911
+SQL Attr Function Style<|||>3<|||>1
+SQL Attr Identifier Foreground<|||>3<|||>32896
+SQL Attr Identifier Background<|||>3<|||>536870911
+SQL Attr Identifier Style<|||>3<|||>0
+SQL Attr Key Foreground<|||>3<|||>16711680
+SQL Attr Key Background<|||>3<|||>536870911
+SQL Attr Key Style<|||>3<|||>1
+SQL Attr Number Foreground<|||>3<|||>8388736
+SQL Attr Number Background<|||>3<|||>536870911
+SQL Attr Number Style<|||>3<|||>0
+SQL Attr PLSQL-ReservedWord Foreground<|||>3<|||>536870911
+SQL Attr PLSQL-ReservedWord Background<|||>3<|||>536870911
+SQL Attr PLSQL-ReservedWord Style<|||>3<|||>1
+SQL Attr ProcName Foreground<|||>3<|||>536870911
+SQL Attr ProcName Background<|||>3<|||>536870911
+SQL Attr ProcName Style<|||>3<|||>0
+SQL Attr Space Foreground<|||>3<|||>536870911
+SQL Attr Space Background<|||>3<|||>536870911
+SQL Attr Space Style<|||>3<|||>0
+SQL Attr SQLPlus-Command Foreground<|||>3<|||>536870911
+SQL Attr SQLPlus-Command Background<|||>3<|||>536870911
+SQL Attr SQLPlus-Command Style<|||>3<|||>1
+SQL Attr String Foreground<|||>3<|||>32768
+SQL Attr String Background<|||>3<|||>536870911
+SQL Attr String Style<|||>3<|||>0
+SQL Attr Symbol Foreground<|||>3<|||>16711680
+SQL Attr Symbol Background<|||>3<|||>536870911
+SQL Attr Symbol Style<|||>3<|||>0
+SQL Attr TableName Foreground<|||>3<|||>16711935
+SQL Attr TableName Background<|||>3<|||>536870911
+SQL Attr TableName Style<|||>3<|||>0
+SQL Attr Variable Foreground<|||>3<|||>8388736
+SQL Attr Variable Background<|||>3<|||>536870911
+SQL Attr Variable Style<|||>3<|||>0
+FieldEditor_Set<|||>3<|||>1
+ColWidths_frmTableEditor.listColumns<|||>1<|||>55,100,95,90,123,111,88,117,130,140,100,100
+ColsVisible_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11
+ColPositions_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11
+ColSort_frmTableEditor.listColumns<|||>1<|||>-1,0
+ColWidths_frmTableEditor.treeIndexes<|||>1<|||>440,100,80
+ColsVisible_frmTableEditor.treeIndexes<|||>1<|||>0,1,2
+ColPositions_frmTableEditor.treeIndexes<|||>1<|||>0,1,2
+ColSort_frmTableEditor.treeIndexes<|||>1<|||>-1,0
+ColWidths_frmTableEditor.listForeignKeys<|||>1<|||>10,537,141,128,80,93
+ColsVisible_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
+ColPositions_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
+ColSort_frmTableEditor.listForeignKeys<|||>1<|||>-1,0
+ColWidths_frmTableEditor.listCheckConstraints<|||>1<|||>200,437
+ColsVisible_frmTableEditor.listCheckConstraints<|||>1<|||>0,1
+ColPositions_frmTableEditor.listCheckConstraints<|||>1<|||>0,1
+ColSort_frmTableEditor.listCheckConstraints<|||>1<|||>-1,0
+MainWinLeft<|||>3<|||>0
+MainWinTop<|||>3<|||>192
+MainWinHeight<|||>3<|||>525
+FileDialogEncoding_MainForm<|||>3<|||>0
+querymemoheight<|||>3<|||>311
+MainWinMaximized<|||>3<|||>1
+MemoEditorWidth<|||>3<|||>136
+sqloutheight<|||>3<|||>30
+MainWinWidth<|||>3<|||>993
+DateTimeEditor_CursorPos_Type4<|||>3<|||>5
+SQLFile0<|||>1<|||>C:\Users\objet\AppData\Roaming\HeidiSQL\Backups\query-tab-2023-05-19_10-10-57-871.sql
+Servers\gdj11\SessionCreated<|||>1<|||>2023-04-18 10:29:52
+Servers\gdj11\Host<|||>1<|||>127.0.0.1
+Servers\gdj11\WindowsAuth<|||>3<|||>0
+Servers\gdj11\CleartextPluginEnabled<|||>3<|||>0
+Servers\gdj11\User<|||>1<|||>root
+Servers\gdj11\Password<|||>1<|||>6F667B66363738395
+Servers\gdj11\LoginPrompt<|||>3<|||>0
+Servers\gdj11\Port<|||>1<|||>3306
+Servers\gdj11\NetType<|||>3<|||>0
+Servers\gdj11\Compressed<|||>3<|||>0
+Servers\gdj11\LocalTimeZone<|||>3<|||>0
+Servers\gdj11\QueryTimeout<|||>3<|||>30
+Servers\gdj11\KeepAlive<|||>3<|||>20
+Servers\gdj11\FullTableStatus<|||>3<|||>1
+Servers\gdj11\Databases<|||>1<|||>diary;employees;homework0419;userboard;fileupload
+Servers\gdj11\Library<|||>1<|||>libmariadb.dll
+Servers\gdj11\Comment<|||>1<|||>
+Servers\gdj11\StartupScriptFilename<|||>1<|||>
+Servers\gdj11\TreeBackground<|||>3<|||>536870911
+Servers\gdj11\SSHtunnelHost<|||>1<|||>
+Servers\gdj11\SSHtunnelHostPort<|||>3<|||>22
+Servers\gdj11\SSHtunnelUser<|||>1<|||>
+Servers\gdj11\SSHtunnelPassword<|||>1<|||>7
+Servers\gdj11\SSHtunnelTimeout<|||>3<|||>4
+Servers\gdj11\SSHtunnelPrivateKey<|||>1<|||>
+Servers\gdj11\SSHtunnelPort<|||>3<|||>3307
+Servers\gdj11\SSL_Active<|||>3<|||>0
+Servers\gdj11\SSL_Key<|||>1<|||>
+Servers\gdj11\SSL_Cert<|||>1<|||>
+Servers\gdj11\SSL_CA<|||>1<|||>
+Servers\gdj11\SSL_Cipher<|||>1<|||>
+Servers\gdj11\IgnoreDatabasePattern<|||>1<|||>
+Servers\gdj11\LogFileDdl<|||>3<|||>0
+Servers\gdj11\LogFileDml<|||>3<|||>0
+Servers\gdj11\LogFilePath<|||>1<|||>C:\Users\objet\AppData\Roaming\HeidiSQL\Logs\%session\%db\%y%m%d.sql
+Servers\gdj11\ServerVersionFull<|||>1<|||>10.5.19-MariaDB - mariadb.org binary distribution
+Servers\gdj11\ConnectCount<|||>3<|||>94
+Servers\gdj11\ServerVersion<|||>3<|||>100519
+Servers\gdj11\LastConnect<|||>1<|||>2023-05-20 19:02:06
+Servers\gdj11\lastUsedDB<|||>1<|||>userboard
+Servers\gdj11\FavoriteObjects<|||>1<|||>diary||notice<{{{><}}}>
+Servers\gdj11\diary|schedule\Sort<|||>1<|||>0_schedule_no|0_schedule_pw|
+Servers\gdj11\diary|schedule\MemoEditorHighlighter_schedule_pw<|||>1<|||>
+Servers\gdj11\diary|student\MemoEditorHighlighter_no<|||>1<|||>
+Servers\gdj11\diary|student\Sort<|||>1<|||>1_name|
+Servers\gdj11\QueryHistory\2<|||>1<|||>2023-04-20 14:34:00|diary|15|DELETE FROM notice WHERE NOTICE_NO = 9;
+Servers\gdj11\QueryHistory\17<|||>1<|||>2023-04-20 14:35:50|diary|0|DELETE FROM notice WHERE NOTICE_NO = 10 AND notice_pw = '1234';
+Servers\gdj11\QueryHistory\19<|||>1<|||>2023-04-20 14:35:59|diary|0|SELECT * FROM notice;
+Servers\gdj11\QueryHistory\18<|||>1<|||>2023-04-24 09:49:26|diary|0|SELECT * FROM SCHEDULE;<{{{><}}}>
+Servers\gdj11\QueryHistory\20<|||>1<|||>2023-04-24 10:02:03|diary|15|SELECT *<{{{><}}}>FROM schedule<{{{><}}}>WHERE schedule_date = '2023-04-22';
+Servers\gdj11\QueryHistory\21<|||>1<|||>2023-04-24 10:02:13|diary|0|SELECT *<{{{><}}}>FROM schedule<{{{><}}}>WHERE schedule_date = '2023-04-21';
+Servers\gdj11\QueryHistory\22<|||>1<|||>2023-04-24 10:03:06|diary|0|SELECT *<{{{><}}}>FROM schedule<{{{><}}}>WHERE schedule_date = '2023-04-20';
+Servers\gdj11\QueryHistory\24<|||>1<|||>2023-04-24 10:12:43|diary|15|SELECT schedule_date, schedule_time, substr(schedule_memo,1,10)<{{{><}}}>FROM schedule<{{{><}}}>WHERE schedule_date = CURDATE()<{{{><}}}>ORDER BY schedule_time ASC;
+Servers\gdj11\QueryHistory\23<|||>1<|||>2023-04-26 10:30:19|employees|0|SELECT<{{{><}}}>	emp_no empNo,<{{{><}}}>	birth_date birthDate,<{{{><}}}>	first_name firstName,<{{{><}}}>	last_name lastName,<{{{><}}}>	gender gender,<{{{><}}}>	hire_date hireDate<{{{><}}}>FROM employees<{{{><}}}>LIMIT 0, 10; -- LIMIT ?, ?
+Servers\gdj11\QueryHistory\25<|||>1<|||>2023-04-27 09:11:38|diary|16|UPDATE schedule SET schedule_pw = '1234'
+Servers\gdj11\QueryHistory\26<|||>1<|||>2023-04-27 09:18:47|employees|0|SELECT * FROM employees<{{{><}}}>WHERE emp_no = 10005;
+Servers\gdj11\QueryHistory\27<|||>1<|||>2023-04-27 09:51:31|employees|219|SELECT concat(first_name,' ',last_name) fullname<{{{><}}}>FROM employees<{{{><}}}>WHERE concat(first_name, ' ', last_name) LIKE '%be%';
+Servers\gdj11\QueryHistory\29<|||>1<|||>2023-04-27 09:58:52|employees|234|SELECT * FROM employees<{{{><}}}>WHERE concat(first_name, last_name) LIKE '%be%';
+Servers\gdj11\QueryHistory\30<|||>1<|||>2023-04-27 10:00:29|employees|234|SELECT concat(first_name,' ',last_name) fullname<{{{><}}}>FROM employees<{{{><}}}>WHERE concat(first_name, ' ', last_name) LIKE '%ab%';
+Servers\gdj11\QueryHistory\31<|||>1<|||>2023-05-02 12:48:15|userboard|16|INSERT INTO member(member_id, member_pw, createdate, updatedate)<{{{><}}}>VALUES('admin', PASSWORD('1234'), NOW(), NOW()); -- 1234를 암호화 시켜서 넣으세요
+Servers\gdj11\QueryHistory\32<|||>1<|||>2023-05-02 12:48:22|userboard|0|SELECT * FROM member WHERE member_id='admin' AND member_pw=PASSWORD('1234');
+Servers\gdj11\QueryHistory\28<|||>1<|||>2023-05-03 10:17:36|userboard|16|INSERT INTO LOCAL VALUES('서울', NOW(), NOW());
+Servers\gdj11\QueryHistory\33<|||>1<|||>2023-05-03 10:17:57|userboard|32|INSERT INTO LOCAL(local_name, createdate, updatedate)VALUES('대전', NOW(), NOW());<{{{><}}}>INSERT INTO LOCAL(local_name, createdate, updatedate)VALUES('대구', NOW(), NOW());<{{{><}}}>INSERT INTO LOCAL(local_name, createdate, updatedate)VALUES('부천', NOW(), NOW());<{{{><}}}>INSERT INTO LOCAL(local_name, createdate, updatedate)VALUES('광명', NOW(), NOW());<{{{><}}}>INSERT INTO LOCAL(local_name, createdate, updatedate)VALUES('성남', NOW(), NOW());
+Servers\gdj11\QueryHistory\34<|||>1<|||>2023-05-03 10:18:01|userboard|0|INSERT INTO LOCAL(local_name, createdate, updatedate)VALUES('인천', NOW(), NOW());
+Servers\gdj11\QueryHistory\35<|||>1<|||>2023-05-03 10:18:27|userboard|15|SELECT * FROM LOCAL;
+Servers\gdj11\QueryHistory\36<|||>1<|||>2023-05-03 10:38:57|userboard|0|INSERT INTO member(member_id, member_pw, createdate, updatedate)<{{{><}}}>VALUES('user1', PASSWORD('1234'), NOW(), NOW());<{{{><}}}>INSERT INTO member(member_id, member_pw, createdate, updatedate)<{{{><}}}>VALUES('user2', PASSWORD('1234'), NOW(), NOW());<{{{><}}}>INSERT INTO member(member_id, member_pw, createdate, updatedate)<{{{><}}}>VALUES('user3', PASSWORD('1234'), NOW(), NOW());<{{{><}}}>INSERT INTO member(member_id, member_pw, createdate, updatedate)<{{{><}}}>VALUES('user4', PASSWORD('1234'), NOW(), NOW());<{{{><}}}>INSERT INTO member(member_id, member_pw, createdate, updatedate)<{{{><}}}>VALUES('user5', PASSWORD('1234'), NOW(), NOW());
+Servers\gdj11\QueryHistory\37<|||>1<|||>2023-05-03 10:39:12|userboard|16|SELECT * FROM member;
+Servers\gdj11\QueryHistory\38<|||>1<|||>2023-05-03 10:57:55|userboard|0|SELECT * FROM board;
+Servers\gdj11\QueryHistory\39<|||>1<|||>2023-05-03 11:01:00|userboard|0|SELECT * FROM local LIMIT 0,1;
+Servers\gdj11\QueryHistory\40<|||>1<|||>2023-05-03 11:03:18|userboard|0|SELECT local_name, '대한민국','박성환'FROM local;
+Servers\gdj11\QueryHistory\41<|||>1<|||>2023-05-03 11:06:12|userboard|0|SELECT local_name localName, '대한민국' country, '박성환' worker FROM local;
+Servers\gdj11\QueryHistory\42<|||>1<|||>2023-05-03 12:15:43|userboard|0|SELECT local_name localName, '대한민국' conuntry,'박성환' worker <{{{><}}}>FROM local <{{{><}}}>LIMIT 0, 1;
+Servers\gdj11\QueryHistory\43<|||>1<|||>2023-05-03 14:12:59|userboard|0|SELECT '전체' localName, COUNT(local_name) cnt FROM board<{{{><}}}>UNION ALL<{{{><}}}>SELECT local_name, COUNT(local_name) FROM board GROUP BY local_name;
+Servers\gdj11\QueryHistory\44<|||>1<|||>2023-05-03 14:15:12|userboard|15|SELECT local_name, COUNT(local_name) FROM board<{{{><}}}>GROUP BY local_name WITH ROLLUP;
+Servers\gdj11\QueryHistory\45<|||>1<|||>2023-05-03 14:16:07|userboard|16|SELECT IFNULL(local_name, '전체'), COUNT(local_name) FROM board<{{{><}}}>GROUP BY local_name WITH ROLLUP; 
+Servers\gdj11\QueryHistory\46<|||>1<|||>2023-05-03 14:28:46|userboard|15|SELECT local_name, MAX(board_no) FROM board GROUP BY local_name;
+Servers\gdj11\QueryHistory\47<|||>1<|||>2023-05-03 14:29:11|userboard|0|SELECT local_name, MAX(board_no) FROM board GROUP BY local_name WITH rollup;
+Servers\gdj11\QueryHistory\48<|||>1<|||>2023-05-03 14:35:53|userboard|16|SELECT *<{{{><}}}>from(SELECT IFNULL(local_name, '전체'), COUNT(local_name) FROM board<{{{><}}}>GROUP BY local_name WITH ROLLUP) t;
+Servers\gdj11\QueryHistory\49<|||>1<|||>2023-05-03 14:37:06|userboard|0|SELECT *<{{{><}}}>from(SELECT IFNULL(local_name, '전체') localName, COUNT(local_name) FROM board<{{{><}}}>GROUP BY local_name WITH ROLLUP) t -- 하나의 테이블로 봄<{{{><}}}>ORDER BY localName DESC;
+Servers\gdj11\QueryHistory\50<|||>1<|||>2023-05-03 14:39:40|userboard|16|SELECT '전체', COUNT(local_name) FROM board<{{{><}}}>UNION<{{{><}}}>SELECT local_name, COUNT(local_name) FROM board GROUP BY local_name;
+Servers\gdj11\QueryHistory\51<|||>1<|||>2023-05-03 14:42:00|userboard|15|SELECT * FROM board where local_name= '광명' LIMIT 0,10;
+Servers\gdj11\QueryHistory\52<|||>1<|||>2023-05-19 10:12:04|fileupload|16|INSERT INTO member<{{{><}}}>VALUES('user1', PASSWORD('1234'), NOW(), NOW());
+Servers\gdj11\QueryHistory\1<|||>1<|||>2023-05-19 10:15:01|fileupload|0|SELECT * FROM member
+Servers\gdj11\QueryHistory\3<|||>1<|||>2023-05-19 15:57:10|fileupload|31|DELETE FROM board;
+Servers\gdj11\userboard|board\MemoEditorHighlighter_board_no<|||>1<|||>
+Servers\gdj11\userboard|comment\Sort<|||>1<|||>1_comment_no|
